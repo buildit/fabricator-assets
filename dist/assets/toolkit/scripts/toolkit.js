7eadec91b4c77979cc0c6b162394f69f7a37af24
Zach Steiner <zsteiner@users.noreply.github.com>
2018-04-26 11:59:03 -0600

    Feature/responsive header (#5)
    * Harden build process
    
    * Responsive header
    
    * Search box
    
    * Transition variables
    
    * Responsive footer work
    
    * Responsive footer

--- dist/assets/toolkit/scripts/toolkit.js
+++ dist/assets/toolkit/scripts/toolkit.js
@@ -55,6 +55,16 @@
 	    $(this).parent().removeClass("hpe-input--active");
 	  }
 	});
+	
+	$('.hpe-nav__menu').click(function () {
+	  $(this).toggleClass('hpe-nav__menu--active');
+	  $('.hpe-header__search').removeClass('hpe-header__search--active');
+	});
+	
+	$('.hpe-nav__search').click(function () {
+	  $('.hpe-header__search').toggleClass('hpe-header__search--active');
+	  $('.hpe-nav__menu').toggleClass('hpe-nav__menu--active');
+	});
 
 /***/ })
 /******/ ]);
--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -9,3 +9,13 @@ $(".hpe-input input").focusout(function(){
     $(this).parent().removeClass("hpe-input--active");
   }
 });
+
+$('.hpe-nav__menu').click(function(){
+  $(this).toggleClass('hpe-nav__menu--active');
+  $('.hpe-header__search').removeClass('hpe-header__search--active');
+});
+
+$('.hpe-nav__search').click(function(){
+  $('.hpe-header__search').toggleClass('hpe-header__search--active');
+  $('.hpe-nav__menu').toggleClass('hpe-nav__menu--active');
+});
