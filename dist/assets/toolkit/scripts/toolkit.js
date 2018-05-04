0d8335098c4aff94a415f9a743ef2f47c111cbe6
Zach Steiner <zsteiner@gmail.com>
2018-05-04 08:28:27 -0600

    Remove console.log

--- dist/assets/toolkit/scripts/toolkit.js
+++ dist/assets/toolkit/scripts/toolkit.js
@@ -50,7 +50,6 @@
 	
 	$(".hpe-input input").focusout(function () {
 	  if ($(this).val().length === 0) {
-	    console.log("input", $(this).val());
 	    $(this).parent().removeClass("hpe-input--active");
 	  }
 	});
--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -6,7 +6,6 @@ $(".hpe-input input").focus(function() {
 
 $(".hpe-input input").focusout(function() {
   if ($(this).val().length === 0) {
-    console.log("input", $(this).val());
     $(this)
       .parent()
       .removeClass("hpe-input--active");
