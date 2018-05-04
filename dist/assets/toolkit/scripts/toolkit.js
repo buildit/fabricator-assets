26224c9110774af24ea559833871bf9a4eb6157a
Zach Steiner <zsteiner@users.noreply.github.com>
2018-05-04 08:29:21 -0600

    Feature/sub category (#10)
    * Mock Data for Subcategory
    
    * Sub-category page
    
    * Page header for subcategory
    
    * Category work
    
    Dynamic Breadcrumbs
    Refactor page header
    
    * Data driven recently viewed
    
    * Filter Sidebar
    
    * Filter Sidebar
    
    * Formatting on filter list
    
    * Basic range input
    
    * Basic Range implementation
    
    * Input range sliders styles
    
    * Product Subcategory Guide
    
    * Checkboxes work on guide
    
    * Remove console.log

--- dist/assets/toolkit/scripts/toolkit.js
+++ dist/assets/toolkit/scripts/toolkit.js
@@ -49,33 +49,65 @@
 	});
 	
 	$(".hpe-input input").focusout(function () {
-	
 	  if ($(this).val().length === 0) {
-	    console.log("input", $(this).val());
 	    $(this).parent().removeClass("hpe-input--active");
 	  }
 	});
 	
-	$('.hpe-nav__menu').click(function () {
-	  $(this).toggleClass('hpe-nav__menu--active');
-	  $('.hpe-header__search').removeClass('hpe-header__search--active');
+	$(".hpe-nav__menu").click(function () {
+	  $(this).toggleClass("hpe-nav__menu--active");
+	  $(".hpe-header__search").removeClass("hpe-header__search--active");
 	});
 	
-	$('.hpe-nav__search').click(function () {
-	  $('.hpe-header__search').toggleClass('hpe-header__search--active');
-	  $('.hpe-nav__menu').toggleClass('hpe-nav__menu--active');
+	$(".hpe-nav__search").click(function () {
+	  $(".hpe-header__search").toggleClass("hpe-header__search--active");
+	  $(".hpe-nav__menu").toggleClass("hpe-nav__menu--active");
 	});
 	
-	$('.hpe-carousel__button--next').click(function () {
-	  $(this).siblings('.hpe-carousel__image--active').removeClass('hpe-carousel__image--active').nextAll('.hpe-carousel__image').first().addClass('hpe-carousel__image--active');
+	$(".hpe-carousel__button--next").click(function () {
+	  $(this).siblings(".hpe-carousel__image--active").removeClass("hpe-carousel__image--active").nextAll(".hpe-carousel__image").first().addClass("hpe-carousel__image--active");
 	});
 	
-	$('.hpe-carousel__button--previous').click(function () {
-	  $(this).siblings('.hpe-carousel__image--active').removeClass('hpe-carousel__image--active').prevAll('.hpe-carousel__image').first().addClass('hpe-carousel__image--active');
+	$(".hpe-carousel__button--previous").click(function () {
+	  $(this).siblings(".hpe-carousel__image--active").removeClass("hpe-carousel__image--active").prevAll(".hpe-carousel__image").first().addClass("hpe-carousel__image--active");
 	});
 	
-	$('.hpe-carousel__indicator').click(function () {
-	  $(this).prev().addClass('hpe-carousel__image--active').siblings().removeClass('hpe-carousel__image--active');
+	$(".hpe-carousel__indicator").click(function () {
+	  $(this).prev().addClass("hpe-carousel__image--active").siblings().removeClass("hpe-carousel__image--active");
+	});
+	
+	$(".hpe-filter__button").click(function () {
+	  $(this).siblings(".hpe-filter__list").toggleClass("hpe-filter--expanded");
+	});
+	
+	$(".hpe-filter__header").click(function () {
+	  const thisSection = $(this).next();
+	
+	  $(this).toggleClass("hpe-filter__header--hidden");
+	
+	  if (thisSection.attr("aria-hidden") === "true") {
+	    thisSection.attr("aria-hidden", false);
+	  } else {
+	    thisSection.attr("aria-hidden", true);
+	  }
+	});
+	
+	// Initialize Input Range
+	
+	$(".hpe-range--handles").each(function (i, obj) {
+	  const thisElement = $(obj);
+	  const minValue = thisElement.attr("data-range-min") ? parseInt(thisElement.attr("data-range-min"), 10) : 0;
+	  const maxValue = thisElement.attr("data-range-max") ? parseInt(thisElement.attr("data-range-max"), 10) : 100;
+	  const minStart = thisElement.attr("data-range-start-min") ? parseInt(thisElement.attr("data-range-start-min"), 10) : 10;
+	  const maxStart = thisElement.attr("data-range-start-max") ? parseInt(thisElement.attr("data-range-start-max"), 10) : 50;
+	
+	  noUiSlider.create(obj, {
+	    range: {
+	      min: minValue,
+	      max: maxValue
+	    },
+	    start: [minStart, maxStart]
+	  });
 	});
 
 /***/ })
--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,107 @@
-const assembler = require('fabricator-assemble');
-const browserSync = require('browser-sync');
-const csso = require('gulp-csso');
-const del = require('del');
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const gulpif = require('gulp-if');
-const imagemin = require('gulp-imagemin');
-const prefix = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
+const assembler = require("fabricator-assemble");
+const browserSync = require("browser-sync");
+const csso = require("gulp-csso");
+const del = require("del");
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+const gulpif = require("gulp-if");
+const imagemin = require("gulp-imagemin");
+const prefix = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
 const reload = browserSync.reload;
-const runSequence = require('run-sequence');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const webpack = require('webpack');
-const merge = require('gulp-merge-json');
-const jsonToSass = require('gulp-json-sass');
-const svgSymbols = require('gulp-svg-symbols');
-const svgmin = require('gulp-svgmin');
+const runSequence = require("run-sequence");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const webpack = require("webpack");
+const merge = require("gulp-merge-json");
+const jsonToSass = require("gulp-json-sass");
+const svgSymbols = require("gulp-svg-symbols");
+const svgmin = require("gulp-svgmin");
 const path = require(`path`);
 
 // configuration
 const config = {
   dev: gutil.env.dev,
   styles: {
-    browsers: 'last 1 version',
+    browsers: "last 1 version",
     fabricator: {
-      src: 'src/assets/fabricator/styles/fabricator.scss',
-      dest: 'dist/assets/fabricator/styles',
-      watch: 'src/assets/fabricator/styles/**/*.scss'
+      src: "src/assets/fabricator/styles/fabricator.scss",
+      dest: "dist/assets/fabricator/styles",
+      watch: "src/assets/fabricator/styles/**/*.scss"
     },
     toolkit: {
-      src: 'src/assets/toolkit/styles/toolkit.scss',
-      dest: 'dist/assets/toolkit/styles',
-      watch: 'src/assets/toolkit/styles/**/*.scss'
+      src: "src/assets/toolkit/styles/toolkit.scss",
+      dest: "dist/assets/toolkit/styles",
+      watch: "src/assets/toolkit/styles/**/*.scss"
     }
   },
   scripts: {
     fabricator: {
-      src: './src/assets/fabricator/scripts/fabricator.js',
-      dest: 'dist/assets/fabricator/scripts',
-      watch: 'src/assets/fabricator/scripts/**/*'
+      src: "./src/assets/fabricator/scripts/fabricator.js",
+      dest: "dist/assets/fabricator/scripts",
+      watch: "src/assets/fabricator/scripts/**/*"
     },
     toolkit: {
-      src: './src/assets/toolkit/scripts/toolkit.js',
-      dest: 'dist/assets/toolkit/scripts',
-      watch: 'src/assets/toolkit/scripts/**/*'
+      src: "./src/assets/toolkit/scripts/toolkit.js",
+      srcScripts: "./src/assets/toolkit/scripts/**/*",
+      dest: "dist/assets/toolkit/scripts",
+      watch: "src/assets/toolkit/scripts/**/*"
     }
   },
   images: {
     toolkit: {
-      src: ['src/assets/toolkit/images/**/*'],
-      dest: 'dist/assets/toolkit/images',
-      watch: 'src/assets/toolkit/images/**/*'
+      src: ["src/assets/toolkit/images/**/*"],
+      dest: "dist/assets/toolkit/images",
+      watch: "src/assets/toolkit/images/**/*"
     }
   },
   icons: {
     toolkit: {
-      src: ['src/assets/toolkit/icons/**/*.svg'],
-      dest: 'dist/assets/toolkit/images',
-      partial: 'src/materials/atoms/icons',
-      watch: 'src/assets/toolkit/icons/**/*',
+      src: ["src/assets/toolkit/icons/**/*.svg"],
+      dest: "dist/assets/toolkit/images",
+      partial: "src/materials/atoms/icons",
+      watch: "src/assets/toolkit/icons/**/*",
       templates: {
-        symbolsLibrary: 'src/views/layouts/includes/f-icons-symbol-library.html',
-        iconLibrary: 'src/views/layouts/includes/f-icons-library.html'
+        symbolsLibrary:
+          "src/views/layouts/includes/f-icons-symbol-library.html",
+        iconLibrary: "src/views/layouts/includes/f-icons-library.html"
       }
     }
   },
   fonts: {
     toolkit: {
-      src: ['src/assets/toolkit/fonts/**/*'],
-      dest: 'dist/assets/toolkit/fonts',
-      watch: 'src/assets/toolkit/fonts/**/*'
+      src: ["src/assets/toolkit/fonts/**/*"],
+      dest: "dist/assets/toolkit/fonts",
+      watch: "src/assets/toolkit/fonts/**/*"
     }
   },
   images: {
     toolkit: {
-      src: ['src/assets/toolkit/images/**/*'],
-      dest: 'dist/assets/toolkit/images',
-      watch: 'src/assets/toolkit/images/**/*'
+      src: ["src/assets/toolkit/images/**/*"],
+      dest: "dist/assets/toolkit/images",
+      watch: "src/assets/toolkit/images/**/*"
     }
   },
   templates: {
-    watch: 'src/**/*.{html,md,json,yml}'
+    watch: "src/**/*.{html,md,json,yml}"
   },
-  dest: 'dist',
+  dest: "dist",
   variables: {
-    jsonVariables: 'src/data/build/variables.json',
-    sassVariables: 'src/assets/toolkit/styles/partials/build/_import-variables.scss',
-    partials: 'src/assets/toolkit/styles/partials/build',
-    dataBuild: 'src/data/build/',
+    jsonVariables: "src/data/build/variables.json",
+    sassVariables:
+      "src/assets/toolkit/styles/partials/build/_import-variables.scss",
+    partials: "src/assets/toolkit/styles/partials/build",
+    dataBuild: "src/data/build/"
   }
 };
 
 // clean
-gulp.task('clean', del.bind(null, [config.dest]));
+gulp.task("clean", del.bind(null, [config.dest]));
 
 // scripts
-const webpackConfig = require('./webpack.config')(config);
+const webpackConfig = require("./webpack.config")(config);
 
-gulp.task('scripts', done => {
+gulp.task("scripts", done => {
   webpack(webpackConfig, (err, stats) => {
     if (err) {
       gutil.log(gutil.colors.red(err()));
@@ -113,58 +116,69 @@ gulp.task('scripts', done => {
   });
 });
 
+// libraries
+gulp.task("libraries", () => {
+  return gulp
+    .src(config.scripts.toolkit.srcScripts, !config.scripts.toolkit.src)
+    .pipe(gulp.dest(config.scripts.toolkit.dest));
+});
+
 // Combine config JSON and Generate Sass Variables
-gulp.task('json', () => {
+gulp.task("json", () => {
   gulp
-    .src('src/data/*.json')
+    .src("src/data/*.json")
     .pipe(
       merge({
         startObj: {},
-        fileName: 'variables.json'
+        fileName: "variables.json"
       })
     )
-    .pipe(gulp.dest('src/data/build'));
+    .pipe(gulp.dest("src/data/build"));
 });
 
-gulp.task('jsonsass', () => {
-  return gulp
-    .src([config.variables.jsonVariables])
-    .pipe(
-      jsonToSass({
-        sass: false,
-        ignoreJsonErrors: true
-      })
-    )
-    // .pipe(concat('_import-variables.scss'))
-    .pipe(rename(function (path) {
-      path.basename = '_import-variables';
-      return path;
-    }))
-    .pipe(gulp.dest(config.variables.partials));
+gulp.task("jsonsass", () => {
+  return (
+    gulp
+      .src([config.variables.jsonVariables])
+      .pipe(
+        jsonToSass({
+          sass: false,
+          ignoreJsonErrors: true
+        })
+      )
+      // .pipe(concat('_import-variables.scss'))
+      .pipe(
+        rename(function(path) {
+          path.basename = "_import-variables";
+          return path;
+        })
+      )
+      .pipe(gulp.dest(config.variables.partials))
+  );
 });
 
 // styles
-gulp.task('styles:fabricator', () => {
+gulp.task("styles:fabricator", () => {
   gulp
     .src(config.styles.fabricator.src)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(prefix(config.styles.browsers))
     .pipe(gulpif(!config.dev, csso()))
-    .pipe(rename('f.css'))
+    .pipe(rename("f.css"))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.styles.fabricator.dest))
     .pipe(gulpif(config.dev, reload({ stream: true })));
 });
 
-gulp.task('styles:toolkit', () => {
+gulp.task("styles:toolkit", () => {
   gulp
     .src(config.styles.toolkit.src)
     .pipe(gulpif(config.dev, sourcemaps.init()))
     .pipe(
       sass({
-        includePaths: './node_modules'
-      }).on('error', sass.logError)
+        includePaths: "./node_modules"
+      }).on("error", sass.logError)
     )
     .pipe(prefix(config.styles.browsers))
     .pipe(gulpif(!config.dev, csso()))
@@ -173,10 +187,10 @@ gulp.task('styles:toolkit', () => {
     .pipe(gulpif(config.dev, reload({ stream: true })));
 });
 
-gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
+gulp.task("styles", ["styles:fabricator", "styles:toolkit"]);
 
 // images
-gulp.task('images', ['favicon'], () => {
+gulp.task("images", ["favicon"], () => {
   return gulp
     .src(config.images.toolkit.src)
     .pipe(imagemin())
@@ -184,47 +198,52 @@ gulp.task('images', ['favicon'], () => {
 });
 
 // icons
-gulp.task('icons', function () {
-  return gulp.src(config.icons.toolkit.src)
-  .pipe(svgmin())  
-  .pipe(svgSymbols(
-      {        
-        slug: function (name) { return `icon-${name}`; },
-        title: 'icon-%f',
+gulp.task("icons", function() {
+  return gulp
+    .src(config.icons.toolkit.src)
+    .pipe(svgmin())
+    .pipe(
+      svgSymbols({
+        slug: function(name) {
+          return `icon-${name}`;
+        },
+        title: "icon-%f",
         svgAttrs: {
-          class: 'svg-icon-lib',
-          'aria-hidden': 'true',
-          style: 'position: absolute;',
-          'data-enabled': true,  
+          class: "svg-icon-lib",
+          "aria-hidden": "true",
+          style: "position: absolute;",
+          "data-enabled": true
         },
         templates: [
-          'default-svg',
+          "default-svg",
           path.join(__dirname, config.icons.toolkit.templates.symbolsLibrary),
-          path.join(__dirname, config.icons.toolkit.templates.iconLibrary),
+          path.join(__dirname, config.icons.toolkit.templates.iconLibrary)
         ]
-      }
-    ))
-    .pipe(rename(function (path) {
-      path.basename = path.basename.replace('f-', '');
-      return path;
-    }))
-    .pipe(gulpif( /[.]svg$/, gulp.dest(config.icons.toolkit.dest)))
-    .pipe(gulpif( /[.]html$/, gulp.dest(config.icons.toolkit.partial)));
+      })
+    )
+    .pipe(
+      rename(function(path) {
+        path.basename = path.basename.replace("f-", "");
+        return path;
+      })
+    )
+    .pipe(gulpif(/[.]svg$/, gulp.dest(config.icons.toolkit.dest)))
+    .pipe(gulpif(/[.]html$/, gulp.dest(config.icons.toolkit.partial)));
 });
 
 // fonts
-gulp.task('fonts', () => {
+gulp.task("fonts", () => {
   return gulp
     .src(config.fonts.toolkit.src)
     .pipe(gulp.dest(config.fonts.toolkit.dest));
- });
+});
 
-gulp.task('favicon', () => {
-  return gulp.src('src/favicon.ico').pipe(gulp.dest(config.dest));
+gulp.task("favicon", () => {
+  return gulp.src("src/favicon.ico").pipe(gulp.dest(config.dest));
 });
 
 // assembler
-gulp.task('assembler', done => {
+gulp.task("assembler", done => {
   assembler({
     logErrors: config.dev,
     dest: config.dest
@@ -233,57 +252,62 @@ gulp.task('assembler', done => {
 });
 
 // server
-gulp.task('serve', () => {
+gulp.task("serve", () => {
   browserSync({
     server: {
       baseDir: config.dest
     },
     notify: false,
-    logPrefix: 'FABRICATOR'
+    logPrefix: "FABRICATOR"
   });
 
-  gulp.task('assembler:watch', ['assembler', 'json', 'jsonsass'], browserSync.reload);
-  gulp.watch(config.templates.watch, ['assembler:watch']);
+  gulp.task(
+    "assembler:watch",
+    ["assembler", "json", "jsonsass"],
+    browserSync.reload
+  );
+  gulp.watch(config.templates.watch, ["assembler:watch"]);
 
-  gulp.task('styles:watch', ['styles']);
+  gulp.task("styles:watch", ["styles"]);
   gulp.watch(
     [config.styles.fabricator.watch, config.styles.toolkit.watch],
-    ['styles:watch']
+    ["styles:watch"]
   );
 
-  gulp.task('scripts:watch', ['scripts'], browserSync.reload);
+  gulp.task("scripts:watch", ["scripts"], browserSync.reload);
   gulp.watch(
     [config.scripts.fabricator.watch, config.scripts.toolkit.watch],
-    ['scripts:watch']
+    ["scripts:watch"]
   );
 
-  gulp.task('images:watch', ['images'], browserSync.reload);
-  gulp.watch(config.images.toolkit.watch, ['images:watch']);
+  gulp.task("images:watch", ["images"], browserSync.reload);
+  gulp.watch(config.images.toolkit.watch, ["images:watch"]);
 
-  gulp.task('icons:watch', ['icons'], browserSync.reload);
-  gulp.watch(config.icons.toolkit.watch, ['icons:watch']);
+  gulp.task("icons:watch", ["icons"], browserSync.reload);
+  gulp.watch(config.icons.toolkit.watch, ["icons:watch"]);
 
-  gulp.task('fonts:watch', ['fonts'], browserSync.reload);
-  gulp.watch(config.fonts.toolkit.watch, ['fonts:watch']);
+  gulp.task("fonts:watch", ["fonts"], browserSync.reload);
+  gulp.watch(config.fonts.toolkit.watch, ["fonts:watch"]);
 });
 
 // default build task
-gulp.task('default', ['clean'], () => {
+gulp.task("default", ["clean"], () => {
   // define build tasks
   const tasks = [
-    'jsonsass',
-    'icons',
-    'scripts',
-    'images',
-    'fonts',
-    'styles',
-    'assembler'
+    "jsonsass",
+    "icons",
+    "scripts",
+    "libraries",
+    "images",
+    "fonts",
+    "styles",
+    "assembler"
   ];
 
   // run build
-  runSequence('json', 'jsonsass', 'icons', tasks, () => {
+  runSequence("json", "jsonsass", "icons", tasks, () => {
     if (config.dev) {
-      gulp.start('serve');
+      gulp.start("serve");
     }
   });
 });
